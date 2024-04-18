a4ec9a17d4ef14994342c4e64dbb13a55357adf4
yannig <ychartois@gmail.com>
2024-04-18 13:42:17 +0100

    Move it as function write unit tests

--- Crypto/crypto.js
+++ Crypto/crypto.js
@@ -1,14 +1,23 @@
 /* Utils */
-var modulo =  (a, b) => {return ((a % b) + b) % b};
-console.log( modulo(-2,26) )
+const modulo =  (a, b) => {return ((a % b) + b) % b};
 
 /* Symmetric Encryption Key */
+const encodeCaesarCypher = (message, Ke) => {
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz'; 
+    return [...message].map( char => [...alphabet][(alphabet.indexOf(char) + Ke) % alphabet.length] ).join('')
+}
+
+const decodeCaesarCypher = (message, Ke) => {
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    return [...message].map(char => [...alphabet][modulo(alphabet.indexOf(char) - Ke, alphabet.length)]).join('');
+}
+
 const Ke = 5;
 const message = "attackatdown";
 const alphabet = 'abcdefghijklmnopqrstuvwxyz';
 
-const encoded = [...message].map( char => [...alphabet][(alphabet.indexOf(char) + Ke) % 26] ).join(''); 
+const encoded = encodeCaesarCypher(message, Ke); 
 console.log(encoded);
 
-const decoded = [...encoded].map(char => [...alphabet][modulo(alphabet.indexOf(char) - Ke, 26)]).join('');
+const decoded = decodeCaesarCypher(encoded, Ke);
 console.log(decoded);
